9252afb1b7703838c865e908492e600bb769a1d7
Demeter-David <demeter.david@student.ms.sapientia.ro>
2025-06-14 15:19:24 +0300

    Corrected some minor bugs
    Ensuring the app runs smooth in any situation.

--- src/dotnet/obj/Debug/net8.0/dotnet.AssemblyInfo.cs
+++ src/dotnet/obj/Debug/net8.0/dotnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6379a1bfd25bca0f30ca217e5d22dada24b16c22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecc50fe7ba009f918dfe05f5fbcd9d0a788de1d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
